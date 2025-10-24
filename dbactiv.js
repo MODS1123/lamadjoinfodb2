aad0a583728608dd626c2f5283d9c87e01d4177d
Lamadjo Info <112094692+MODS1123@users.noreply.github.com>
2025-10-24 05:34:09 +0500

    Add auth code: XX0N for user Alberto114

--- dbactiv.js
+++ dbactiv.js
@@ -1,1 +1,14 @@
-
+{
+  "codes": [
+    {
+      "code": "XX0N",
+      "timestamp": "2025-10-24T00:34:09.132Z",
+      "used": false,
+      "userData": {
+        "telegramId": 921182112,
+        "username": "Alberto114",
+        "nickname": "Alberto114"
+      }
+    }
+  ]
+}
